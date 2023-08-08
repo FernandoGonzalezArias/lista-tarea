1c57d56ea6ae7ed0068a78e3947f785fdab05516
Fernando Gonzalez Arias <efernando.g94@gmail.com>
2023-08-07 21:55:28 -0400

    up

--- administrar/admin.py
+++ administrar/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from administrar.models import Tarea
 
-# Register your models here.
+class  TareaAdmin (admin.ModelAdmin):
+    list_display = ("titulo", "estado", "descripcion")
+    
+    def descripcion(self, obj):
+        if obj.estado:
+            return "Completado"
+        else:
+            return "Pendiente"
+
+admin.site.register(Tarea, TareaAdmin)
